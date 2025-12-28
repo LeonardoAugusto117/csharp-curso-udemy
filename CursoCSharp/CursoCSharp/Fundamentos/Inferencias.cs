2f3539b28f7c8c310547d53a9182fbe39680db87
leonardo CASA <leonardodevaugusto2d@gmail.com>
2025-12-28 15:36:37 -0300

    Subindo Inferencias Tipos

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Inferencias.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Inferencias
+    {
+        public static void Executar() {
+
+            var nome = "Leonardo";
+            Console.WriteLine(nome);
+
+           /* "var numero;" <--- Desta maneira o compilador informarar o erro, segue abaixo uma possivel solução.
+            * Necessario sempre que for usar o VAR informar o valor que aquela variavel recebera para que o compilador
+            * Saiba o que está sendo referenciado nela.
+            */
+            var numero = 0;
+            numero = 10;
+            Console.WriteLine(numero);
+
+            int a;
+            int b = 3;
+
+            int soma = a = 2 + b;
+            Console.WriteLine(soma);
+
+
+
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -10,6 +10,7 @@ namespace CursoCSharp {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferencias - Fundamentos", Inferencias.Executar},
             });
 
             central.SelecionarEExecutar();
